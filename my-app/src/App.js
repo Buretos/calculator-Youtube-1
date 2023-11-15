02ba9cea422989bccabf11d32f97897db61b8ea2
Aleksandr Morozov <moroz.cuporos@gmail.com>
2023-11-15 15:05:02 +0300

    Adding comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import { useState } from 'react';
 
 function App() {
+	// Инициализация состояния калькулятора
 	const [calc, setCalc] = useState('');
+	// Инициализация состояния результата
 	const [result, setResult] = useState('');
 
+	// Операторы калькулятора
 	const ops = ['/', '*', '+', '-', '.'];
 
-	const updateCalc = (volue) => {
+	// Функция для обновления состояния калькулятора
+	const updateCalc = (value) => {
+		// Проверяем, возможность добавления оператора
 		if (
-			(ops.includes(volue) && calc === '') ||
-			(ops.includes(volue) && ops.includes(calc.slice(-1)))
+			(ops.includes(value) && calc === '') || // Если оператор первый символ или
+			(ops.includes(value) && ops.includes(calc.slice(-1))) // если предыдущий символ уже оператор
 		) {
-			return;
+			return; // Возвращаемся, не обновляя состояние
 		}
 
-		setCalc(calc + volue);
+		// Обновляем состояние калькулятора, добавляя введенный символ
+		setCalc(calc + value);
 
-		if (!ops.includes(volue)) {
-			setResult(eval(calc + volue).toString());
+		// Проверяем, является ли символ оператором
+		if (!ops.includes(value)) {
+			// Если символ не оператор, вычисляем результат и обновляем состояние результата
+			setResult(eval(calc + value).toString());
 		}
 	};
 
+	// Функция для создания кнопок с цифрами
 	const createDigits = () => {
 		const digits = [];
 
@@ -35,17 +44,22 @@ function App() {
 		return digits;
 	};
 
+	// Функция для выполнения вычислений
 	const calculate = () => {
+		// Вычисляем результат и обновляем состояние результата
 		setCalc(eval(calc).toString());
 	};
 
+	// Функция для удаления последнего символа
 	const deleteLast = () => {
-		if (calc == '') {
-			return;
+		if (calc === '') {
+			// Проверяем, пустая ли строка
+			return; // Возвращаемся, нет символов для удаления
 		}
 
-		const value = calc.slice(0, -1);
+		const value = calc.slice(0, -1); // Удаляем последний символ
 
+		// Обновляем состояние калькулятора
 		setCalc(value);
 	};
 
@@ -54,23 +68,30 @@ function App() {
 			<div className="calculator">
 				<div className="display">
 					{result ? <span>({result})</span> : ''}&nbsp;
-					{calc || '0'}
+					{calc || '0'} {/* Если есть результат выводим его, иначе - 0 */}
 				</div>
 
 				<div className="operators">
-					<button onClick={() => updateCalc('/')}>/</button>
-					<button onClick={() => updateCalc('*')}>*</button>
-					<button onClick={() => updateCalc('+')}>+</button>
-					<button onClick={() => updateCalc('-')}>-</button>
-
-					<button onClick={deleteLast}>DEL</button>
+					<button onClick={() => updateCalc('/')}>/</button>{' '}
+					{/* Обработчик клика для добавления оператора / */}
+					<button onClick={() => updateCalc('*')}>*</button>{' '}
+					{/* Обработчик клика для добавления оператора * */}
+					<button onClick={() => updateCalc('+')}>+</button>{' '}
+					{/* Обработчик клика для добавления оператора + */}
+					<button onClick={() => updateCalc('-')}>-</button>{' '}
+					{/* Обработчик клика для добавления оператора - */}
+					<button onClick={deleteLast}>DEL</button>{' '}
+					{/* Обработчик клика для удаления последнего символа */}
 				</div>
 
 				<div className="digits">
-					{createDigits()}
-					<button onClick={() => updateCalc('0')}>0</button>
-					<button onClick={() => updateCalc('.')}>.</button>
-					<button onClick={calculate}>=</button>
+					{createDigits()} {/* Создаем кнопки с цифрами */}
+					<button onClick={() => updateCalc('0')}>0</button>{' '}
+					{/* Обработчик клика для добавления цифры 0 */}
+					<button onClick={() => updateCalc('.')}>.</button>{' '}
+					{/* Обработчик клика для добавления десятичной точки */}
+					<button onClick={calculate}>=</button>{' '}
+					{/* Обработчик клика для выполнения вычислений */}
 				</div>
 			</div>
 		</div>
